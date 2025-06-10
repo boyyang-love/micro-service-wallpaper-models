3ecb4a1e46471dcae362d9a7f2518bd197aca64a
boyyang <boyyang-love@outlook.com>
2025-06-10 18:18:12 +0800

    feat: 新增模型 :sparkles:

---
+++ models/feedback.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+	"strings"
+)
+
+type Feedback struct {
+	Base
+	FeedbackId string `json:"feedback_id" form:"feedback_id"`
+	Content    string `json:"Content" form:"Content"`
+	Type       string `json:"type" form:"type"`
+	UserId     string `json:"user_id" form:"user_id"`
+}
+
+func (f *Feedback) TableName() string {
+	return "feedback"
+}
+
+func (f *Feedback) BeforeCreate(db *gorm.DB) (err error) {
+	uid := uuid.NewV1()
+	f.Id = strings.Replace(uid.String(), "-", "", -1)
+	return nil
+}
