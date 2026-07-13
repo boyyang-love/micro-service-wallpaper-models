6c9c7d14ca59d87a84f5fff08abad85901b76adc
boyyang <62252841+boyyang-love@users.noreply.github.com>
2026-07-13 13:13:29 +0800

    feat: add DailyWallpaper model for daily wallpaper feature

---
+++ models/daily.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"strings"
+
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+)
+
+type DailyWallpaper struct {
+	Base
+	UploadId    string `json:"upload_id" form:"upload_id" gorm:"column:upload_id"`
+	Date        string `json:"date" form:"date" gorm:"column:date;uniqueIndex"`
+	Description string `json:"description" form:"description" gorm:"column:description"`
+	Edition     int    `json:"edition" form:"edition" gorm:"column:edition"`
+	Status      int    `json:"status" form:"status" gorm:"column:status;default:1"`
+}
+
+func (d *DailyWallpaper) TableName() string {
+	return "daily_wallpaper"
+}
+
+func (d *DailyWallpaper) BeforeCreate(db *gorm.DB) (err error) {
+	uid := uuid.NewV1()
+	d.Id = strings.Replace(uid.String(), "-", "", -1)
+	return nil
+}
