e07090a710ce4e3250f6f3be488e1ca2e94cf9b8
boyyang <62252841+boyyang-love@users.noreply.github.com>
2026-07-18 11:09:33 +0800

    feat: 新增post模型 :sparkles:

---
+++ models/post.go
@@ -0,0 +1,26 @@
+package models
+
+import (
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+	"strings"
+)
+
+type Post struct {
+	Base
+	UserId   string `json:"user_id" form:"user_id"`
+	Title    string `json:"title" form:"title"`
+	Content  string `json:"content" form:"content"`
+	ImageIds string `json:"image_ids" form:"image_ids"` // 逗号分隔的 upload id
+	Status   int    `json:"status" form:"status" gorm:"default:1"` // 0=草稿 1=已发布 2=审核中
+}
+
+func (p *Post) TableName() string {
+	return "post"
+}
+
+func (p *Post) BeforeCreate(db *gorm.DB) (err error) {
+	uid := uuid.NewV1()
+	p.Id = strings.Replace(uid.String(), "-", "", -1)
+	return nil
+}
